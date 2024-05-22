e46abeba4f22bc75e661a647908a076d546bde21
wilsongit078 <149802240+wilsongit078@users.noreply.github.com>
2024-05-22 10:54:07 +0300

    Add files via upload

---
+++ academic.php
@@ -0,0 +1,9 @@
+<?php echo "my academic profile <br>"; 
+
+$Name  ="Wilson";
+$Program ="Computer Science";
+$Skills = "Python";
+echo "My name is $Name <br> ";
+echo "I have done $Program <br>";
+echo "My area of specialization is $Skills <br>";
+?>
